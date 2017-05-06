6d84485b0abe251e1fa97bd0cd16a98e3461ec18
tdvance <tdvance@gmail.com>
2017-05-05 21:35:01 -0400

    Unity 2017: State of Project at Beginning of Chapter 8

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float speed = 10.0f;
-    
+
+    private MazeBuilder maze;
+    private Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        int dx, dy;
-        GetInput(out dx, out dy);
-        Move(dx, dy);
-	}
+        maze = FindObjectOfType<MazeBuilder>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (maze) {
+            int dx, dy;
+            GetInput(out dx, out dy);
+            Move(dx, dy);
+        }
+    }
 
     void GetInput(out int dx, out int dy) {
         dx = 0;
         dy = 0;
+
+        int x = Mathf.RoundToInt(transform.position.x);
+        int y = Mathf.RoundToInt(transform.position.y);
+
+
         if (Input.GetAxis("Horizontal") < 0) {
             dx = -1;
         } else if (Input.GetAxis("Horizontal") > 0) {
@@ -30,15 +41,25 @@ public class PlayerController : MonoBehaviour {
         } else if (Input.GetAxis("Vertical") > 0) {
             dy = 1;
         }
+
+        if (!maze.IsPlayerSpace(x + dx, y + dy)) {
+            dx = 0;
+            dy = 0;
+        }
     }
 
-    void Move(int dx, int dy) {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        if (!rb) {
-            throw new UnityException("Missing Rigidbody2D component");
-        }
+    void Move(int dx, int dy) {       
         if (dx != 0 || dy != 0) {
             rb.velocity = new Vector2(dx, dy) * speed;
+        }
+        //correct position
+        if (dx != 0) {
+            int y = Mathf.RoundToInt(transform.position.y);
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        }
+        if (dy != 0) {
+            int x = Mathf.RoundToInt(transform.position.x);
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
         }
     }
 }
