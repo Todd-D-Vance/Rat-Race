8d7e1a4574245e7e952c51505614169931c1282e
tdvance <tdvance@gmail.com>
2017-05-09 10:35:23 -0400

    Unity 2017: State of Project at Beginning of Chapter 20

---
+++ Assets/Scripts/AttractMode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AttractMode : MonoBehaviour {
+
+    public float timeRemaining = 10.0f;
+    public string nextScene = "Description";
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 0) {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+
+    public void PlayGame() {
+        SceneManager.LoadScene("Game");
+    }
+}
